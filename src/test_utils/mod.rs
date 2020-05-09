c7993699ecf75b70ca5ebb5d136b3e0ed07190f7
Jing Yang <ditsing@gmail.com>
2020-05-09 08:25:56 -0700

    Re-arrange macro order in test_utils.

--- src/test_utils/mod.rs
+++ src/test_utils/mod.rs
@@ -2,20 +2,20 @@ pub mod local_tcp_server;
 pub mod plaintext_crypter;
 pub mod ready_buf;
 
-macro_rules! crypto_vec {
-    ($ ($ x : expr), *) => {
-        vec![$($x),*]
-    };
-    ($ ($ x : expr,) *) => {
-        vec![$($x,)*]
-    };
-}
-
 macro_rules! crypto_array {
     ($ ($ x : expr), *) => {
         [$($x),*]
     };
     ($ ($ x : expr,) *) => {
         [$($x,)*]
+    };
+}
+
+macro_rules! crypto_vec {
+    ($ ($ x : expr), *) => {
+        vec![$($x),*]
+    };
+    ($ ($ x : expr,) *) => {
+        vec![$($x,)*]
     };
 }
